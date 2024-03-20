05b746ee1397cb0aaaf6af1387edad985beaba79
Isolde Boussauw <isolde.boussauw@student.vives.be>
2024-03-20 12:44:10 +0100

    verwijderen van een migraineaanval is nu mogelijk

--- migraine_tracker/urls.py
+++ migraine_tracker/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('migraine', views.overzicht, name='overzicht'),  # Overzicht van migraineaanvallen
+    path('migraine/', views.overzicht, name='overzicht'),  # Overzicht van migraineaanvallen
     path('migraine/Add/', views.add_migraine_aanval, name='add_migraine_aanval'),  # Pagina om een nieuwe migraineaanval toe te voegen
+    path('migraine/Delete/<int:aanval_id>/', views.delete_migraine_aanval, name='delete_migraine_aanval'),
 ]
--- migraine_tracker/views.py
+++ migraine_tracker/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import MigraineAanvalForm
-from .models import MigraineAanval, Symptoom, Trigger
+from .models import MigraineAanval
 
 def add_migraine_aanval(request):
     if request.method == 'POST':
@@ -37,3 +37,16 @@ def add_migraine_aanval(request):
 def overzicht(request):
     aanvallen = MigraineAanval.objects.all()
     return render(request, 'migraine/overzicht.html', {'aanvallen': aanvallen})
+
+def delete_migraine_aanval(request, aanval_id):
+    # Haal de migraineaanval op uit de database
+    aanval = get_object_or_404(MigraineAanval, pk=aanval_id)
+    
+    if request.method == 'POST':
+        # Als het een POST-verzoek is, verwijder de aanval
+        aanval.delete()
+        # Redirect naar het overzichtspagina na verwijdering
+        return redirect('overzicht')
+    
+    # Render het bevestigingsvenster voor het verwijderen van de aanval
+    return render(request, 'migraine/delete_migraine_aanval.html', {'aanval': aanval})
